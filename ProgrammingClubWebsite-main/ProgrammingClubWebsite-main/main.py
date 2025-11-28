a77c537a319cc55d922d0b0cc494d4ff99f8217f
ccwang15 <zihan.wang15@education.nsw.gov.au>
2025-11-28 11:34:12 +1100

    Add files via upload

--- ProgrammingClubWebsite-main/ProgrammingClubWebsite-main/main.py
+++ ProgrammingClubWebsite-main/ProgrammingClubWebsite-main/main.py
@@ -3,10 +3,30 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/justins-home.html')
+@app.route('/home.html')
 @app.route('/')
 def home():
-    return render_template('justins-home.html')
+    return render_template('home.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/comps.html')
+def comps():
+    return render_template('comps.html')
+
+@app.route('/registration.html')
+def registration():
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run(debug= True)
